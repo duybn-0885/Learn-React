721c9feb4024c810b628f62772e544292b38ad01
oBuiNhatDuy <bui.nhat.duy@sun-asterisk.com>
2020-06-03 15:12:20 +0700

    Add conditional rendering

--- src/index.js
+++ src/index.js
@@ -36,8 +36,98 @@ class Clock extends React.Component{
   }
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
+  }
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign in</h1>
+}
+
+function Gretting(props) {
+  const isLoggedIn = props.isLoggedIn;
+
+  if(isLoggedIn) {
+    return <UserGreeting />
+  }
+  return <GuestGreeting />
+}
+
+function LoginButton(props) {
+  return(
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return(
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return(
+      <div>
+        <Gretting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Clock />,
+  <LoginControl />,
   document.getElementById('root')
 )
 serviceWorker.unregister();
